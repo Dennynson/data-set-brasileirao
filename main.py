f8fdbf91cbccfdc04f2af3762b1d4d1c56625cdf
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2025-03-11 10:37:37 -0300

    Criada função do csv

--- main.py
+++ main.py
@@ -19,21 +19,40 @@ def collect_raw_league_standings(league_id, start_season, end_season):
     for year in range(start_season, end_season + 1):
         data = get_football_data("standings", {"league": league_id, "season": year})
         
-        if ("response" in data and data["response"]):
+        if("response" in data and data["response"]):
             raw_data[year] = data["response"]
     
     return raw_data
 
+def save_standings_to_csv(standings, filename):
+    with open(filename, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Temporada","Posição", "Time", "Pontos", "Saldo de Gols", "Jogos", "Vitórias", "Empates", "Derrotas"])
+            
+        for season, data in standings.items():
+            for league in data:
+                for team in league['league']['standings'][0]:
+                    writer.writerow([
+                        season,
+                        team["rank"],
+                        team["team"]["name"],
+                        team["points"],
+                        team["goalsDiff"],
+                        team["all"]["played"],
+                        team["all"]["win"],
+                        team["all"]["draw"],
+                        team["all"]["lose"]
+                        ])
+
+
 def main():
-    start_season = 2000
+    start_season = 2021
     end_season = 2023
     league_id = 71  # Brasileirão Série A
     
     raw_standings_data = collect_raw_league_standings(league_id, start_season, end_season)
     
-    print("Dados crus das últimas temporadas(2000 - 2023) do Brasileirão Série A:")
-    for year, data in raw_standings_data.items():
-        print(f"Temporada {year}:", data)
+    save_standings_to_csv(raw_standings_data, "brasileirao_standings.csv")
 
 if (__name__ == "__main__"):
    main()
